17e29950590a4bab5d81288c14bec134d019c234
Johan Higuita <higuitajohan@gmail.com>
2019-01-16 23:48:19 -0500

    Check Fields console funcionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function() {
 
 
+	// Render Slides:
 	$.ajax({
 		method: "GET",
 		url: "https://api.myjson.com/bins/17ocpi"
@@ -10,9 +11,17 @@ $(function() {
 		alert("Error");
 	});
 
+	//Console Comboboxes:
+	$( ".select-input" ).change(function() {
+		var msg = '<div class="console-cont text-light">Question change: ..... <span>'+someYes()+'</span></div>';
+		$(".console").append(msg);	
+	});
+
 // ---------------------------- functions --------------------------------------
 
 function renderSliders(data) {
+	//Render de Values from the API in teh sliders
+
 	slide_1 = data['slide_1'];
 	slide_2 = data['slide_2'];
 	slide_3 = data['slide_3'];
@@ -28,6 +37,18 @@ function renderSliders(data) {
 	$("#content_slide1").text(slide_1.text);
 	$("#content_slide2").text(slide_2.text);
 	$("#content_slide3").text(slide_3.text);
+}
+
+function someYes() {
+	//There is at least one "YES" in the selects values
+
+	var arr_selects = $('.select-input');
+	for (var i = 0; i < arr_selects.length; i ++) {
+		if(arr_selects[i].value == "YES"){
+			return "true";
+		}
+	}
+	return "false";	
 }
 
 
