a92653b24a0a59f1470a856535152393a1a6e47a
Johan Higuita <higuitajohan@gmail.com>
2019-01-16 20:11:17 -0500

    Rendering slides information from the API

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
 $(function() {
-    //console.log( "ready!" );
+
+
+	$.ajax({
+		method: "GET",
+		url: "https://api.myjson.com/bins/17ocpi"
+	}).done(function(data) {
+		renderSliders(data);
+	}).fail(function() {
+		alert("Error");
+	});
+
+// ---------------------------- functions --------------------------------------
+
+function renderSliders(data) {
+	slide_1 = data['slide_1'];
+	slide_2 = data['slide_2'];
+	slide_3 = data['slide_3'];
+	// images:
+	$("#img_slide1").attr("src",slide_1.image);
+	$("#img_slide2").attr("src",slide_2.image);
+	$("#img_slide3").attr("src",slide_3.image);
+	//Titles:
+	$("#title_slide1").text(slide_1.title);
+	$("#title_slide2").text(slide_2.title);
+	$("#title_slide3").text(slide_3.title);
+	//content texts:
+	$("#content_slide1").text(slide_1.text);
+	$("#content_slide2").text(slide_2.text);
+	$("#content_slide3").text(slide_3.text);
+}
+
+
 
 });
