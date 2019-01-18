7ea0aae4918e9065d9e8b4e0b6306cef4a25d115
Johan Higuita <higuitajohan@gmail.com>
2019-01-17 23:04:36 -0500

    Implements Media Queries for manual design responsive

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 $(function() {
 
-
+	$(this).scrollTop(0); // start in the top when user refresh
 	// Render Slides:
-
 	$.getJSON("https://api.myjson.com/bins/17ocpi", function(data) {
 		renderSliders({sliders: data});
 	})
