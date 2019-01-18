22a5695d5bee0fb8bc7db12fa7c3e4a6a449d0b2
Johan Higuita <higuitajohan@gmail.com>
2019-01-17 23:38:33 -0500

    Modularization of JS files

---
+++ js/functions.js
@@ -0,0 +1,46 @@
+
+
+// ---------------------------- functions --------------------------------------
+
+function renderSliders(data) {
+	//render Sliders using Handlebars
+
+	var template = Handlebars.compile($('#slider-template').html());
+	var html = template(data);
+	// Add the compiled html to the page
+	$('.render-sliders').html(html);
+	$('.cont-item:first-child').addClass("active"); //active first slider
+	setBold(); // set bold last word in title
+}
+
+function someYes() {
+	//There is at least one "YES" in the selects values
+
+	var arr_selects = $('.select-input');
+	for (var i = 0; i < arr_selects.length; i ++) {
+		if(arr_selects[i].value == "YES"){
+			return "true";
+		}
+	}
+	return "false";	
+}
+
+function setBold(argument) {
+	var arr_titles = $(".title_slide").map(function(){
+		//Modify array adding <b></b> last word
+		var title = $(this).text();
+		var arr = title.split(" ");
+		var lastword = arr[arr.length -1];
+		var lw_mod = "<b>"+lastword+"</b>";
+		arr.pop();
+		var str = arr.join(" ");
+		$(this).html(str +" "+ lw_mod);
+		return str +" "+ lw_mod;
+	}).get();
+	
+}
+
+Handlebars.registerHelper('toUpperCase', function(str) {
+	//Title slider Uppercase
+	return str.toUpperCase();
+});
